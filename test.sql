054dc2029e3cb1e2e6a6787428033e27da9dda4a
Viral Thakar <vrl.thakar@gmail.com>
2025-04-21 23:41:54 +0530

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,1 +1,16 @@
-LS @SNOWFLAKE_EXTENSIONS/branches/main;
+CREATE OR REPLACE PROCEDURE my_python_proc()
+RETURNS STRING
+LANGUAGE PYTHON
+RUNTIME_VERSION = '3.8'
+PACKAGES = ('snowflake-snowpark-python')
+HANDLER = 'main'
+AS
+'  
+import snowflake.snowpark as snowpark
+from snowflake.snowpark.functions import col
+
+def main(session: snowpark.Session): 
+    print("hello")
+
+    return "hello"
+';
